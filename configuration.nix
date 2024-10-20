7a0305e8f10f2b3b9cb145c85ca00736c5b95366
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-10-20 18:25:15 +0300

    chore

--- configuration.nix
+++ configuration.nix
@@ -213,6 +213,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+  hardware.graphics.enable32Bit = true;
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
 
@@ -231,7 +232,6 @@
     amdgpuBusId = "PCI:8:0:0";
   };
 
-  hardware.opengl.driSupport32Bit = true;
   services.kanata = {
     enable = true;
     keyboards = {
