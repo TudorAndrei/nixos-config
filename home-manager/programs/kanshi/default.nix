1fe8da2e68f0e87bb510c14b052cc88136072248
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-12-23 19:07:45 +0200

    kanshi

---
+++ home-manager/programs/kanshi/default.nix
@@ -0,0 +1,30 @@
+{}: {
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "hyprland-session.target";
+    profiles = {
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            scale = 1.0;
+            status = "enable";
+          }
+        ];
+      };
+      home = {
+        outputs = [
+          {
+            criteria = "LG Electronics LG ULTRAGEAR 0x00023FE9";
+            position = "0,0";
+            mode = "2560x1080@143.94Hz";
+          }
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+        ];
+      };
+    };
+  };
+}
