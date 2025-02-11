1cf3412e1ff77f2842797a0fc71916e93e6b2264
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2025-02-11 22:11:47 +0200

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,7 @@
 {
-  config,
   inputs,
+  outputs,
+  config,
   pkgs,
   unstablePkgs,
   ...
@@ -17,6 +18,22 @@
     ./programs/easyeffects
   ];
   nixpkgs = {
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
     config = {
       allowUnfree = true;
     };
@@ -101,7 +118,7 @@
       ldtk
       brave
       wireshark
-      unstablePkgs.windsurf
+      unstable.windsurf
     ];
   };
 
