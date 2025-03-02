f689c539b6b2f7301cf18ac9aa2f5bd6b3647bf0
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2025-03-02 20:14:38 +0200

    add librewolf

--- home-manager/programs/firefox/default.nix
+++ home-manager/programs/firefox/default.nix
@@ -130,6 +130,17 @@
       };
     };
   };
+  programs.librewolf = {
+    enable = true;
+    # Enable WebGL, cookies and history
+    settings = {
+      "webgl.disabled" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "network.cookie.lifetimePolicy" = 0;
+    };
+  };
 
   xdg.mimeApps.defaultApplications = {
     "x-scheme-handler/http" = ["firefox.desktop"];
