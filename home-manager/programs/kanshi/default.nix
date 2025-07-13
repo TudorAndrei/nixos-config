e96be61f9f099a7b55278b747d4b5dcd60012fc0
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2025-07-13 18:56:13 +0300

    update

--- home-manager/programs/kanshi/default.nix
+++ home-manager/programs/kanshi/default.nix
@@ -4,33 +4,20 @@
     systemdTarget = "graphical-session.target";
     # systemdTarget = "hyprland-session.target";
     profiles = {
-      home_1 = {
+      home_monitor = {
         outputs = [
           {
             criteria = "LG Electronics LG ULTRAGEAR 0x00023FE9";
             position = "0,0";
-            mode = "2560x1080@60Hz";
+            mode = "2560x1080@144Hz";
           }
           {
-            criteria = "eDP-1";
+            criteria = "BOE NE173QHM-NZ2";
             status = "disable";
           }
         ];
       };
-      home_2 = {
-        outputs = [
-          {
-            criteria = "LG Electronics LG ULTRAGEAR 0x00023FE9";
-            position = "0,0";
-            mode = "2560x1080@60Hz";
-          }
-          {
-            criteria = "eDP-2";
-            status = "disable";
-          }
-        ];
-      };
-      home_bigmonitor = {
+      home_tv = {
         outputs = [
           {
             criteria = "LG Electronics LG TV SSCR2 0x01010101";
@@ -38,20 +25,7 @@
             mode = "3840x2160@60.0Hz";
           }
           {
-            criteria = "eDP-1";
-            status = "disable";
-          }
-        ];
-      };
-      home_bigmonitor2 = {
-        outputs = [
-          {
-            criteria = "LG Electronics LG TV SSCR2 0x01010101";
-            position = "0,0";
-            mode = "3840x2160@60.0Hz";
-          }
-          {
-            criteria = "eDP-2";
+            criteria = "BOE NE173QHM-NZ2";
             status = "disable";
           }
         ];
@@ -65,23 +39,7 @@
             scale = 2.0;
           }
           {
-            criteria = "eDP-1";
-            scale = 1.0;
-            status = "enable";
-            position = "1920,0";
-          }
-        ];
-      };
-      work2 = {
-        outputs = [
-          {
-            criteria = "Dell Inc. DELL U4320Q 41LFCH3";
-            position = "0,0";
-            mode = "3840x2160@60Hz";
-            scale = 2.0;
-          }
-          {
-            criteria = "eDP-1";
+            criteria = "BOE NE173QHM-NZ2";
             scale = 1.0;
             status = "enable";
             position = "1920,0";
