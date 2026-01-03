5fa099c799b09bff1d59c9d70af9227fcfeb3151
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2026-01-03 21:37:23 +0200

    Add EasyEffects configuration files and update symlink paths
    - Introduced new configuration files for EasyEffects, including presets and equalizer settings.
    - Updated symlink paths in the default.nix file to point to the new database and data directories.
    - Added various output configurations for different audio devices and presets to enhance audio processing capabilities.

--- home-manager/programs/easyeffects/default.nix
+++ home-manager/programs/easyeffects/default.nix
@@ -4,7 +4,13 @@
   };
   xdg.configFile = {
     "easyeffects" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/programs/easyeffects/config";
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/programs/easyeffects/config/db";
+      recursive = true;
+    };
+  };
+  xdg.dataFile = {
+    "easyeffects" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/programs/easyeffects/data";
       recursive = true;
     };
   };
